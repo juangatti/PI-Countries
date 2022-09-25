d3c5ac42c6874f451358951c4307b00519535890
juangatti <Juanse10028@gmail.com>
2022-09-25 12:16:34 -0300

    solving issues in landing page

---
+++ client/build/static/js/3.eca49f7c.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpclient=this.webpackJsonpclient||[]).push([[3],{171:function(n,e,t){"use strict";t.r(e),t.d(e,"default",(function(){return c}));t(1);var r=t(83),i=t(144),a=t(0);function c(n){var e=n.currentCountry;return Object(a.jsx)(i.a.Container,{gap:2,justify:"center",align:"center",children:e&&e.map((function(n){return Object(a.jsx)(r.a,{id:n.id,region:n.region,flag:n.flag,name:n.name},n.id)}))})}}}]);
+//# sourceMappingURL=3.eca49f7c.chunk.js.map
--- client/src/components/LandingPage/landingpage.jsx
+++ client/src/components/LandingPage/landingpage.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import  s from "./landingpage.module.css"
+import s from "./landingpage.module.css"
 
 
 
@@ -8,17 +8,17 @@ export function LandingP() {
     return (
         <div className={s.all}>
             <div className={s.titlec}>
-            <h1 className={s.title}>  WorldApp!</h1>
+                <h1 className={s.title}>  WorldApp!</h1>
             </div>
             <div className={s.container}>
                 <p className={s.text2}>Join us, create your own activities and explore the world...</p>
                 <Link to="/countries">
-                <button  className="btn btn-outline-warning">Ingresar</button>
-            </Link>
+                    <button >Ingresar</button>
+                </Link>
             </div>
             <div>
             </div>
         </div>
-        
+
     )
 }
